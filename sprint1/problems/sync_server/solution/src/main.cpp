9eb42c4eb9be4b701e0b2529d95875b05ff5eb26
StasNe <sstusne@yandex.ru>
2023-09-10 13:20:19 +0000

    sync_server

---
+++ sprint1/problems/sync_server/solution/src/main.cpp
@@ -0,0 +1,145 @@
+#ifdef WIN32
+#include <sdkddkver.h>
+#endif
+// boost.beast будет использовать std::string_view вместо boost::string_view
+#define BOOST_BEAST_USE_STD_STRING_VIEW
+
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <iostream>
+#include <optional>
+#include <thread>
+
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+using namespace std::literals;
+namespace beast = boost::beast;
+namespace http = beast::http;
+// Запрос, тело которого представлено в виде строки
+using StringRequest = http::request<http::string_body>;
+// Ответ, тело которого представлено в виде строки
+using StringResponse = http::response<http::string_body>;
+struct ContentType
+{
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;
+    // При необходимости внутрь ContentType можно добавить и другие типы контента
+};
+std::optional<StringRequest> ReadRequest(tcp::socket& socket, beast::flat_buffer& buffer)
+{
+    beast::error_code ec;
+    StringRequest req;
+    // Считываем из socket запрос req, используя buffer для хранения данных.
+    // В ec функция запишет код ошибки.
+    http::read(socket, buffer, req, ec);
+
+    if (ec == http::error::end_of_stream)
+    {
+        return std::nullopt;
+    }
+    if (ec)
+    {
+        throw std::runtime_error("Failed to read request: "s.append(ec.message()));
+    }
+    return req;
+}
+
+// Создаёт StringResponse с заданными параметрами
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version, bool keep_alive,
+                                  std::string_view content_type = ContentType::TEXT_HTML)
+{
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+void DumpRequest(const StringRequest& req)
+{
+    std::cout << req.method_string() << ' ' << req.target() << std::endl;
+    // Выводим заголовки запроса
+    for (const auto& header : req)
+    {
+        std::cout << "  "sv << header.name_string() << ": "sv << header.value() << std::endl;
+    }
+}
+
+StringResponse HandleRequest(StringRequest&& req)
+{
+    std::cout << "METHOD :" << req.method() << std::endl;
+    if (req.method() != http::verb::get && req.method() != http::verb::head)
+    {
+        return MakeStringResponse(http::status::method_not_allowed, "Invalid method"sv, req.version(),
+                                  req.keep_alive());
+    }
+    const auto text_response = [&req](http::status status, std::string_view text)
+    {
+        return MakeStringResponse(status, text, req.version(), req.keep_alive());
+    };
+
+    std::string resp{"Hello, "};
+    resp += req.target().substr(1, req.target().size() - 1);
+    return text_response(http::status::ok, resp);
+}
+template <typename RequestHandler>
+void HandleConnection(tcp::socket& socket, RequestHandler&& handle_request)
+{
+    try
+    {
+        // Буфер для чтения данных в рамках текущей сессии.
+        beast::flat_buffer buffer;
+
+        // Продолжаем обработку запросов, пока клиент их отправляет
+        while (auto request = ReadRequest(socket, buffer))
+        {
+            DumpRequest(*request);
+
+            auto response = handle_request(*std::move(request));
+            // Отправляем ответ сервера клиенту
+            http::write(socket, response);
+
+            // Прекращаем обработку запросов, если семантика ответа требует это
+            if (response.need_eof())
+            {
+                break;
+            }
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    beast::error_code ec;
+    // Запрещаем дальнейшую отправку данных через сокет
+    socket.shutdown(tcp::socket::shutdown_send, ec);
+}
+
+int main()
+{
+    net::io_context ioc;
+    const auto address = net::ip::make_address("0.0.0.0");
+    constexpr unsigned short port = 8080;
+
+    // Объект, позволяющий принимать tcp-подключения к сокету
+    tcp::acceptor acceptor(ioc, {address, port});
+
+    std::cout << "Server has started..."sv << std::endl;
+    while (true)
+    {
+        tcp::socket socket(ioc);
+        acceptor.accept(socket);
+        // Запускаем обработку взаимодействия с клиентом в отдельном потоке
+        std::thread t(
+            [](tcp::socket socket)
+            {
+                // Вызываем HandleConnection, передавая ей функцию-обработчик запроса
+                HandleConnection(socket, HandleRequest);
+            },
+            std::move(socket));
+        t.detach();
+    }
+}
