2e0b81ebdbecce260f2940848cc9d72aa3922066
StasNe <sstusne@yandex.ru>
2023-09-17 19:28:38 +0000

    async_server copy-paste

--- sprint1/problems/async_server/solution/src/main.cpp
+++ sprint1/problems/async_server/solution/src/main.cpp
@@ -8,7 +8,8 @@
 
 #include "http_server.h"
 
-namespace {
+namespace
+{
 namespace net = boost::asio;
 using namespace std::literals;
 namespace sys = boost::system;
@@ -19,16 +20,17 @@ using StringRequest = http::request<http::string_body>;
 // Ответ, тело которого представлено в виде строки
 using StringResponse = http::response<http::string_body>;
 
-struct ContentType {
+struct ContentType
+{
     ContentType() = delete;
     constexpr static std::string_view TEXT_HTML = "text/html"sv;
     // При необходимости внутрь ContentType можно добавить и другие типы контента
 };
 
 // Создаёт StringResponse с заданными параметрами
-StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
-                                  bool keep_alive,
-                                  std::string_view content_type = ContentType::TEXT_HTML) {
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version, bool keep_alive,
+                                  std::string_view content_type = ContentType::TEXT_HTML)
+{
     StringResponse response(status, http_version);
     response.set(http::field::content_type, content_type);
     response.body() = body;
@@ -37,49 +39,65 @@ StringResponse MakeStringResponse(http::status status, std::string_view body, un
     return response;
 }
 
-StringResponse HandleRequest(StringRequest&& req) {
-    // Подставьте сюда код из синхронной версии HTTP-сервера
-    return MakeStringResponse(http::status::ok, "OK"sv, req.version(), req.keep_alive());
-}
-
 // Запускает функцию fn на n потоках, включая текущий
 template <typename Fn>
-void RunWorkers(unsigned n, const Fn& fn) {
+void RunWorkers(unsigned n, const Fn& fn)
+{
     n = std::max(1u, n);
     std::vector<std::jthread> workers;
     workers.reserve(n - 1);
     // Запускаем n-1 рабочих потоков, выполняющих функцию fn
-    while (--n) {
+    while (--n)
+    {
         workers.emplace_back(fn);
     }
     fn();
 }
 
-}  // namespace
+StringResponse HandleRequest(StringRequest&& req)
+{
+    std::cout << "METHOD :" << req.method() << std::endl;
+    if (req.method() != http::verb::get && req.method() != http::verb::head)
+    {
+        return MakeStringResponse(http::status::method_not_allowed, "Invalid method"sv, req.version(),
+                                  req.keep_alive());
+    }
+    const auto text_response = [&req](http::status status, std::string_view text)
+    {
+        return MakeStringResponse(status, text, req.version(), req.keep_alive());
+    };
 
-int main() {
+    std::string resp{"Hello, "};
+    resp += req.target().substr(1, req.target().size() - 1);
+    return text_response(http::status::ok, resp);
+}
+
+}  // namespace
+int main()
+{
     const unsigned num_threads = std::thread::hardware_concurrency();
 
     net::io_context ioc(num_threads);
 
     // Подписываемся на сигналы и при их получении завершаем работу сервера
     net::signal_set signals(ioc, SIGINT, SIGTERM);
-    signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
-        if (!ec) {
-            ioc.stop();
-        }
-    });
+    signals.async_wait(
+        [&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number)
+        {
+            if (!ec)
+            {
+                std::cout << "Пользователь завершил выполнение программы\n";
+                ioc.stop();
+            }
+        });
 
     const auto address = net::ip::make_address("0.0.0.0");
     constexpr net::ip::port_type port = 8080;
-    http_server::ServeHttp(ioc, {address, port}, [](auto&& req, auto&& sender) {
-        // sender(HandleRequest(std::forward<decltype(req)>(req)));
-    });
+    http_server::ServeHttp(ioc, {address, port},
+                           [](auto&& req, auto&& sender) { sender(HandleRequest(std::forward<decltype(req)>(req))); });
 
     // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
     std::cout << "Server has started..."sv << std::endl;
 
-    RunWorkers(num_threads, [&ioc] {
-        ioc.run();
-    });
+    RunWorkers(num_threads, [&ioc] { ioc.run(); });
 }
