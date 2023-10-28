d5c385878d8c291747e9e829e975c89b6f306a5b
StasNe <sstusne@yandex.ru>
2023-10-28 23:10:42 +0000

    s3 flamegraph

---
+++ sprint3/problems/flamegraph/solution/src/request_handler.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/json.hpp>
+#include <string_view>
+#include "http_server.h"
+#include "model.h"
+#include "router.h"
+namespace http_handler {
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+
+class RequestHandler {
+public:
+    explicit RequestHandler(model::Game& game) : game_{game} { SetupRoutes(); }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        router_.Route(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+    }
+
+private:
+    void SetupRoutes();
+
+    void get_map_handler(const Router::Request& request, Router::Response& response) const;
+    void get_maps_list_handler(const Router::Request& request, Router::Response& response) const;
+
+private:
+    model::Game& game_;
+    Router router_;
+};
+
+}  // namespace http_handler
