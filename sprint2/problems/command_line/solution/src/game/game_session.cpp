6c3130e17edf3cb867fdbb8f763f15d83856f129
StasNe <sstusne@yandex.ru>
2023-10-29 15:09:00 +0000

    refactoring

--- sprint2/problems/command_line/solution/src/game/game_session.cpp
+++ sprint2/problems/command_line/solution/src/game/game_session.cpp
@@ -6,6 +6,25 @@ static uint32_t SessionId{0};
 using namespace model;
 namespace game {
 
+RealPoint BoundDogMovementToMap(const RealPoint start, const RealPoint& finish, const Map& map) {
+    auto possibleRoads = map.GetRoadsForPoint(start);
+
+    if (!possibleRoads.size()) {
+        //dog not on road???
+        boost::json::value json{{"map", map.GetName()}, {"x", start.x}, {"y", start.y}};
+        Logger::Log(json, "Dog not on road!");
+        return start;
+    }
+
+    RealPoint tmpNextPoint = possibleRoads.front().FitPointToRoad(finish);
+    for (const auto& road : possibleRoads) {
+        auto roadBoundPoint = road.FitPointToRoad(finish);
+        if (start.VectorLength(roadBoundPoint) > start.VectorLength(tmpNextPoint))
+            tmpNextPoint = roadBoundPoint;
+    }
+    return tmpNextPoint;
+}
+
 RealPoint GetSpawnPoint(const Map& map, bool isRandom) {
     auto& roads = map.GetRoads();
     if (!roads.size())
@@ -21,15 +40,18 @@ RealPoint GetSpawnPoint(const Map& map, bool isRandom) {
             static_cast<double>(bound(chosenRoad.GetStart().y, chosenRoad.GetEnd().y, seed))};
 }
 
-GameSession::GameSession(SessionConfiguration&& config) : id_(SessionId++), config_(std::move(config)) {
-    config_.name = config_.name.empty() ? config_.map.GetName() + '_' + std::to_string(id_) : std::string(config_.name);
-}
+GameSession::GameSession(SessionConfiguration&& config)
+    : id_(SessionId++),
+      name_(config.name.empty() ? config.map.GetName() + '_' + std::to_string(id_) : config.name),
+      map_(config.map),
+      speed_(config.speed),
+      randomSpawn_(config.randomSpawnPoint) {}
 
 void GameSession::AddDog(const spDog doge) {
     dogs_.push_back(doge);
-    auto mapSpawnPoint = GetSpawnPoint(config_.map, config_.randomSpawnPoint);
+    auto mapSpawnPoint = GetSpawnPoint(map_, randomSpawn_);
     dogs_.back()->SetPosition(mapSpawnPoint);
-    auto roadsForPoint = config_.map.GetRoadsForPoint(mapSpawnPoint);
+    auto roadsForPoint = map_.GetRoadsForPoint(mapSpawnPoint);
 }
 
 void GameSession::DogAction(uint32_t dogId, DogDirection action) {
@@ -37,7 +59,7 @@ void GameSession::DogAction(uint32_t dogId, DogDirection action) {
     if (foundDog == dogs_.end()) {
         return;
     }
-    (*foundDog)->SetDirection(action, config_.speed);
+    (*foundDog)->SetDirection(action, speed_);
 }
 
 void GameSession::UpdateState(uint32_t tick_ms) {
@@ -46,30 +68,11 @@ void GameSession::UpdateState(uint32_t tick_ms) {
 void GameSession::UpdateDogsPosition(uint32_t tick_ms) {
     for (auto& spdog : dogs_) {
         RealPoint estimatePosition = spdog->EstimatePosition(tick_ms);
-        auto boundPoint = BoundDogMovementToMap(spdog->Position(), estimatePosition);
+        auto boundPoint = BoundDogMovementToMap(spdog->Position(), estimatePosition, map_);
         spdog->SetPosition(boundPoint);
         if (boundPoint != estimatePosition)
             spdog->SetSpeed(0);
     }
-}
-
-RealPoint GameSession::BoundDogMovementToMap(const RealPoint start, const RealPoint& finish) const {
-    auto possibleRoads = config_.map.GetRoadsForPoint(start);
-
-    if (!possibleRoads.size()) {
-        //dog not on road???
-        boost::json::value json{{"map", config_.map.GetName()}, {"x", start.x}, {"y", start.y}};
-        Logger::Log(json, "Dog not on road!");
-        return start;
-    }
-
-    RealPoint tmpNextPoint = possibleRoads.front().FitPointToRoad(finish);
-    for (const auto& road : possibleRoads) {
-        auto roadBoundPoint = road.FitPointToRoad(finish);
-        if (start.VectorLength(roadBoundPoint) > start.VectorLength(tmpNextPoint))
-            tmpNextPoint = roadBoundPoint;
-    }
-    return tmpNextPoint;
 }
 
 }  // namespace game
--- sprint2/problems/command_line/solution/src/game/game_session.h
+++ sprint2/problems/command_line/solution/src/game/game_session.h
@@ -21,9 +21,9 @@ public:
     GameSession(SessionConfiguration&& config);
     GameSession(const GameSession&) = delete;
     GameSession(GameSession&&) = delete;
-    std::string_view GetName() const noexcept { return config_.name; }
+    std::string_view GetName() const noexcept { return name_; }
     uint32_t GetId() const { return id_; }
-    const model::Map& GetMap() const { return config_.map; }
+    const model::Map& GetMap() const { return map_; }
 
     void AddDog(const spDog doge);
     void DogAction(uint32_t dogId, DogDirection action);
@@ -36,12 +36,13 @@ public:
 private:
     void UpdateDogsPosition(uint32_t tick_ms);
 
-    RealPoint BoundDogMovementToMap(const RealPoint start, const RealPoint& finish) const;
-
 private:
     uint32_t id_;
     Dogs dogs_;
-    SessionConfiguration config_;
+    std::string name_;
+    const model::Map& map_;
+    const double speed_;
+    const bool randomSpawn_;
 };
 
 using spGameSession = std::shared_ptr<GameSession>;
