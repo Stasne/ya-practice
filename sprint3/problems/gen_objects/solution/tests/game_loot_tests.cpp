381e72cc2342ddc39d0200fc865b42c74ca0d4d6
StasNe <sstusne@yandex.ru>
2023-11-11 13:22:49 +0000

    removed comments

--- sprint3/problems/gen_objects/solution/tests/game_loot_tests.cpp
+++ sprint3/problems/gen_objects/solution/tests/game_loot_tests.cpp
@@ -54,54 +54,5 @@ SCENARIO("Game loot spawning") {
                 REQUIRE(spSession->GetLoot().size() == 2);
             }
         }
-        // WHEN("Half a tick = no loot") {
-        //     THEN("no loot till time tick") {
-
-        //     }
-        // }
-
-        // WHEN("number of looters exceeds loot count") {
-        //     THEN("number of loot is proportional to loot difference") {
-        //         for (unsigned loot = 0; loot < 10; ++loot) {
-        //             for (unsigned looters = loot; looters < loot + 10; ++looters) {
-        //                 INFO("loot count: " << loot << ", looters: " << looters);
-        //                 REQUIRE(gen.Generate(TIME_INTERVAL, loot, looters) == looters - loot);
-        //             }
-        //         }
-        //     }
-        // }
     }
-
-    // GIVEN("a loot generator with some probability") {
-    //     constexpr TimeInterval BASE_INTERVAL = 1s;
-    //     LootGenerator gen{BASE_INTERVAL, 0.5};
-
-    //     WHEN("time is greater than base interval") {
-    //         THEN("number of generated loot is increased") {
-    //             CHECK(gen.Generate(BASE_INTERVAL * 2, 0, 4) == 3);
-    //         }
-    //     }
-
-    //     WHEN("time is less than base interval") {
-    //         THEN("number of generated loot is decreased") {
-    //             const auto time_interval = std::chrono::duration_cast<TimeInterval>(
-    //                 std::chrono::duration<double>{1.0 / (std::log(1 - 0.5) / std::log(1.0 - 0.25))});
-    //             CHECK(gen.Generate(time_interval, 0, 4) == 1);
-    //         }
-    //     }
-    // }
-
-    // GIVEN("a loot generator with custom random generator") {
-    //     LootGenerator gen{1s, 0.5, [] {
-    //                           return 0.5;
-    //                       }};
-    //     WHEN("loot is generated") {
-    //         THEN("number of loot is proportional to random generated values") {
-    //             const auto time_interval = std::chrono::duration_cast<TimeInterval>(
-    //                 std::chrono::duration<double>{1.0 / (std::log(1 - 0.5) / std::log(1.0 - 0.25))});
-    //             CHECK(gen.Generate(time_interval, 0, 4) == 0);
-    //             CHECK(gen.Generate(time_interval, 0, 4) == 1);
-    //         }
-    //     }
-    // }
 }
