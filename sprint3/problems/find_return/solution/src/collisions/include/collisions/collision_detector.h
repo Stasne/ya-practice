0c15e244cf275141e5e97c64183fba57fc90b01b
StasNe <sstusne@yandex.ru>
2023-11-21 17:59:14 +0000

    find_return init

---
+++ sprint3/problems/find_return/solution/src/collisions/include/collisions/collision_detector.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "geom.h"
+
+#include <algorithm>
+#include <vector>
+
+namespace collision_detector {
+
+struct CollectionResult {
+    bool IsCollected(double collect_radius) const {
+        return proj_ratio >= 0 && proj_ratio <= 1 && sq_distance <= collect_radius * collect_radius;
+    }
+    // Квадрат расстояния до точки
+    double sq_distance;
+    // Доля пройденного отрезка
+    double proj_ratio;
+};
+
+// Движемся из точки a в точку b и пытаемся подобрать точку c
+CollectionResult TryCollectPoint(geom::Point2D a, geom::Point2D b, geom::Point2D c);
+
+struct Item {
+    geom::Point2D position;
+    double        width;
+};
+
+struct Gatherer {
+    uint32_t      ingame_id;
+    geom::Point2D start_pos;
+    geom::Point2D end_pos;
+    double        width;
+};
+
+class IItemGathererProvider {
+protected:
+    ~IItemGathererProvider() = default;
+
+public:
+    virtual size_t   ItemsCount() const            = 0;
+    virtual Item     GetItem(size_t idx) const     = 0;
+    virtual size_t   GatherersCount() const        = 0;
+    virtual Gatherer GetGatherer(size_t idx) const = 0;
+};
+
+class ItemGathererProvider : public IItemGathererProvider {
+public:
+    ItemGathererProvider(std::vector<Item> items, std::vector<Gatherer> daugs)
+        : items_(std::move(items)), dogs_(std::move(daugs)) {}
+    size_t   ItemsCount() const override { return items_.size(); }
+    Item     GetItem(size_t idx) const override { return items_.at(idx); }
+    size_t   GatherersCount() const override { return dogs_.size(); }
+    Gatherer GetGatherer(size_t idx) const override { return dogs_.at(idx); }
+
+private:
+    std::vector<Item>     items_;
+    std::vector<Gatherer> dogs_;
+};
+
+struct GatheringEvent {
+    size_t item_id;
+    size_t gatherer_id;
+    double sq_distance;
+    double time;
+};
+
+std::vector<GatheringEvent> FindGatherEvents(const IItemGathererProvider& provider);
+
+}  // namespace collision_detector
---
+++ sprint3/problems/find_return/solution/src/game/game_session.cpp
@@ -0,0 +1,107 @@
+#include "game_session.h"
+#include <logger.h>
+#include <model/map.h>
+#include <random>
+namespace {
+static uint32_t SessionId{0};
+}
+using namespace model;
+namespace game {
+uint32_t GenerateRandomUint(size_t max) {
+    if (max == 1)
+        return 0;
+    std::random_device                                       dev;
+    std::mt19937                                             rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, max - 1);
+    return dist6(rng);
+};
+RealPoint BoundDogMovementToMap(const RealPoint start, const RealPoint& finish, const Map& map) {
+    auto possibleRoads = map.GetRoadsForPoint(start);
+
+    if (!possibleRoads.size()) {
+        //dog not on road???
+        boost::json::value json{{"map", map.GetName()}, {"x", start.x}, {"y", start.y}};
+        Logger::Log(json, "Dog not on road!");
+        return start;
+    }
+
+    RealPoint tmpNextPoint = possibleRoads.front().FitPointToRoad(finish);
+    for (const auto& road : possibleRoads) {
+        auto roadBoundPoint = road.FitPointToRoad(finish);
+        if (start.VectorLength(roadBoundPoint) > start.VectorLength(tmpNextPoint))
+            tmpNextPoint = roadBoundPoint;
+    }
+    return tmpNextPoint;
+}
+
+RealPoint GetNextMapPoint(const Map& map, bool isRandom = true) {
+    auto& roads = map.GetRoads();
+    if (!roads.size())
+        return {0, 0};
+
+    if (!isRandom)
+        return {static_cast<double>(roads.front().GetStart().x), static_cast<double>(roads.front().GetStart().y)};
+
+    static uint32_t seed{0};
+    const auto&     chosenRoad = roads[GenerateRandomUint(roads.size())];
+    uint32_t        lowestX    = std::min(chosenRoad.GetStart().x, chosenRoad.GetEnd().x);
+    uint32_t        highestX   = std::max(chosenRoad.GetStart().x, chosenRoad.GetEnd().x);
+    uint32_t        lowestY    = std::min(chosenRoad.GetStart().y, chosenRoad.GetEnd().y);
+    uint32_t        highestY   = std::max(chosenRoad.GetStart().y, chosenRoad.GetEnd().y);
+
+    uint32_t roadLength = std::max(highestX - lowestX, highestY - lowestY);
+    auto     spawnX     = std::clamp(lowestX + GenerateRandomUint(roadLength), lowestX, highestX);
+    auto     spawnY     = std::clamp(lowestY + GenerateRandomUint(roadLength), lowestY, highestY);
+    return {static_cast<double>(spawnX), static_cast<double>(spawnY)};
+}
+
+GameSession::GameSession(SessionConfiguration&& config)
+    : id_(SessionId++),
+      name_(config.name.empty() ? config.map.GetName() + '_' + std::to_string(id_) : config.name),
+      map_(config.map),
+      speed_(config.speed),
+      bagCapacity_(config.bagCapacity),
+      randomSpawn_(config.randomSpawnPoint),
+      lootGen_(std::chrono::seconds(config.randomGeneratorPeriod), config.randomGeneratorProbability) {}
+
+void GameSession::AddDog(const spDog doge) {
+    dogs_.push_back(doge);
+    auto mapSpawnPoint = GetNextMapPoint(map_, randomSpawn_);
+    dogs_.back()->SetPosition(mapSpawnPoint);
+}
+
+void GameSession::DogAction(uint32_t dogId, DogDirection action) {
+    auto foundDog = std::find_if(dogs_.begin(), dogs_.end(), [dogId](auto& dog) { return dog->Id() == dogId; });
+    if (foundDog == dogs_.end())
+        return;
+
+    (*foundDog)->SetDirection(action, speed_);
+}
+
+void GameSession::UpdateState(uint32_t tick_ms) {
+    UpdateDogsPosition(tick_ms);
+    //pick loot (if possible)
+    //remove picked loot
+    SpawnLoot(tick_ms);
+}
+void GameSession::UpdateDogsPosition(uint32_t tick_ms) {
+    for (auto& spdog : dogs_) {
+        RealPoint estimatePosition = spdog->EstimatePosition(tick_ms);
+        auto      boundPoint       = BoundDogMovementToMap(spdog->Position(), estimatePosition, map_);
+        spdog->SetPosition(boundPoint);
+        if (boundPoint != estimatePosition)
+            spdog->SetSpeed(0);
+    }
+}
+void GameSession::SpawnLoot(uint32_t tick_ms) {
+    auto lootToSpawn = lootGen_.Generate(std::chrono::milliseconds(tick_ms), lootPositions_.size(), dogs_.size());
+    if (!lootToSpawn)
+        return;
+    static uint32_t lootNum;
+    for (auto i = 0; i < lootToSpawn; ++i) {
+        auto lootType = GenerateRandomUint(map_.GetLootTypes().size());
+        lootPositions_.insert({lootNum++, {GetNextMapPoint(map_), lootType}});
+    }
+}
+
+}  // namespace game
